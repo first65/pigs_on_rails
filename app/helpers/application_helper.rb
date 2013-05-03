d1a17177f2b15fad075892c23a59917fd1c39899
christhesoul <chris@first65.com>
2013-05-03 20:32:47 +0100

    Pig status is slaughtered when pig is included in slaughter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def alive_id
     Health.find_by_name('Alive').id
   end
+  
+  def slaughtered_id
+    Health.find_by_name('Slaughtered!').id
+  end
 end
--- app/models/slaughter.rb
+++ app/models/slaughter.rb
@@ -1,4 +1,13 @@
 class Slaughter < ActiveRecord::Base
   attr_accessible :butcher_id, :transport_id, :pig_ids
   has_many :pigs
+  after_create :kill_pigs
+  
+  include ApplicationHelper
+  
+  def kill_pigs
+    pigs.each do |pig|
+      pig.update_attributes(status: slaughtered_id)
+    end
+  end
 end
